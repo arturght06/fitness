9505a6823840ccc33768da5ae824307e5989fe1e
arturg <01194240@pw.edu.pl>
2024-12-27 19:41:15 +0100

    added User table

---
+++ src/main/java/com/example/demo/User.java
@@ -0,0 +1,43 @@
+package com.example.demo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "\"Users\"")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id", nullable = false)
+    private Integer userId;
+
+    @Column(name = "login", nullable = false, length = 30)
+    private String login;
+
+    @Column(name = "password_hash", nullable = false, length = 64)
+    private String passwordHash;
+
+    public Integer getUserId() {
+        return userId;
+    }
+
+    public void setUserId(Integer userId) {
+        this.userId = userId;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getPasswordHash() {
+        return passwordHash;
+    }
+
+    public void setPasswordHash(String passwordHash) {
+        this.passwordHash = passwordHash;
+    }
+
+}
