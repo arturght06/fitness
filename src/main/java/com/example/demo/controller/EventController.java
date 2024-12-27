ca09016164f57521c3bdf3f0e492fc9eae84aa4e
arturg <01194240@pw.edu.pl>
2024-12-27 20:06:42 +0100

    repositories

--- src/main/java/com/example/demo/controller/EventController.java
+++ src/main/java/com/example/demo/controller/EventController.java
@@ -1,6 +1,5 @@
 package com.example.demo.controller;
 
-import com.example.demo.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
---
+++ src/main/java/com/example/demo/repository/KlubyLekkoatletyczneRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+import com.example.demo.model.KlubyLekkoatletyczne;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KlubyLekkoatletyczneRepository extends JpaRepository<KlubyLekkoatletyczne, Integer> {
+}
